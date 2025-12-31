5976c43f38637c363c66cd9cb6a3084038a3edd7
sanghyun97 <sanjungl97@naver.com>
2025-12-31 14:05:22 +0900

    오류수정

--- database/phthon/config.py
+++ database/phthon/config.py
@@ -1,6 +1,6 @@
-hostip='172.16.250.193',
-hostuser='root',
-hostpassword='qwer1234',
+hostip='172.16.250.193'
+hostuser='root'
+hostpassword='qwer1234'
 hostdatabase='teamproject'
 
 userAddress = '172.16.250.193' #<<<<<<<<<<<<< 각자 아이피 넣고 진행하면 됩니다.
