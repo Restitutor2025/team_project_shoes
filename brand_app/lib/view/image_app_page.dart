4537104086348750ce4429c1e4302fd6675d3990
sion <jx010102@naver.com>
2025-12-31 13:58:38 +0900

    sion 이미지 수정 언제끝날까

--- brand_app/lib/view/image_app_page.dart
+++ brand_app/lib/view/image_app_page.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:brand_app/util/pcolor.dart';
-import 'package:brand_app/util/snackbar.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageAppPage extends StatefulWidget {
@@ -14,350 +12,249 @@ class ImageAppPage extends StatefulWidget {
 }
 
 class _ImageAppPageState extends State<ImageAppPage> {
-  final ImagePicker _picker = ImagePicker(); // 이미지
+  final ImagePicker _picker = ImagePicker();
 
-  // 이미지를 담을 변수 (DB 저장 전 단계)
-  XFile? _mainImage;
-  XFile? _detailImage;
+  File? mainImage;
+  File? topImage;
+  File? sideImage;
+  File? backImage;
 
-  // 이미지 선택 함수
-  Future<void> _pickImage(bool isMain) async {
-    final XFile? selected = await _picker.pickImage(
+  ///
+  ///// 제조사 테스트 데이터 (나중에 DB로 교체)
+  final List<String> manufacturers = [
+    '삼성',
+    'LG',
+    'Apple',
+    'Sony',
+  ];
+
+  String? selectedManufacturer;
+
+  // 상품명 컨트롤러
+  final TextEditingController productNameController =
+      TextEditingController();
+
+  ///////////////
+  Future<void> _pickImage(Function(File) onSelected) async {
+    final XFile? picked = await _picker.pickImage(
       source: ImageSource.gallery,
+      // 이미지 안정화 버전
+      maxWidth: 1024,
+      maxHeight: 1024,
+      imageQuality: 85,
     );
-    if (selected != null) {
-      setState(() {
-        if (isMain)
-          _mainImage = selected;
-        else
-          _detailImage = selected;
-      });
-      CustomSnackbar().okSnackBar(
-        "알림",
-        "${isMain ? '대표' : '상세'} 이미지가 선택되었습니다.",
-      );
+
+    if (picked != null) {
+      onSelected(File(picked.path));
+      setState(() {});
     }
   }
 
-  String? selectedCategory; // 카테고리
-  String? selectedPrice; // 가격
-  String? selectedColor; // 색깔
-  String? selectedSize; // 사이즈
-  String? selectedBrand; // 제조사
-
-  // 더미데이터(나중에 DB로 교제함)
-  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  final List<String> categoryList = ['운동화', '슬리퍼', '구두'];
-
-  final List<String> priceList = [
-    '50,000',
-    '100,000',
-    '150,000',
-  ];
-
-  final List<String> colorList = ['블랙', '화이트', '그레이'];
-
-  final List<String> sizeList = [
-    '230',
-    '240',
-    '250',
-    '260',
-  ];
-
-  final List<String> brandList = ['나이키', '아디다스', '푸마'];
-  /////////>>>>>>>>>>>>>>>>>>>>>
-  ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('상품등록'),
+        title: const Text('상품등록'),
+        centerTitle: true,
         backgroundColor: Pcolor.appBarBackgroundColor,
         foregroundColor: Pcolor.appBarForegroundColor,
         elevation: 0,
-        centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: SizedBox(
-            width: 500,
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.start,
-                children: [
-                  //
-                  _dropdown(
-                    label: '제품명',
-                    value: selectedCategory, //카테고리 선택
-                    items: categoryList, // 상품 카테고리
-                    onChanged: (value) {
-                      //
-                      selectedCategory = value;
-                      setState(() {});
-                    },
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  '*  ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.red,
                   ),
-                  _dropdown(
-                    label: '가격',
-                    value: selectedPrice, //가격 선택
-                    items: priceList, // 가격 카테고리
-                    onChanged: (value) {
-                      //
-                      selectedPrice = value;
-                      setState(() {});
-                    },
+                ),
+                Text(
+                  '상품이미지',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
-                  _dropdown(
-                    label: '컬러',
-                    value: selectedColor, //드랍다운 선택
-                    items: colorList, // 상품 카테고리
-                    onChanged: (value) {
-                      //
-                      selectedColor = value;
-                      setState(() {});
-                    },
-                  ),
-                  _dropdown(
-                    label: '사이즈',
-                    value: selectedSize, //드랍다운 선택
-                    items: sizeList, // 상품 카테고리
-                    onChanged: (value) {
-                      //
-                      selectedSize = value;
-                      setState(() {});
-                    },
-                  ),
-                  _dropdown(
-                    label: '제조사',
-                    value: selectedBrand, //드랍다운 선택
-                    items: brandList, // 상품 카테고리
-                    onChanged: (value) {
-                      //
-                      selectedBrand = value;
-                      setState(() {});
-                    },
-                  ),
-                  SizedBox(height: 30),
-                  Center(
-                    child: Text(
-                      '이미지 가져오기',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
+                ),
 
-                  Row(
-                    children: [
-                      Expanded(
-                        child: _buildImagePreview(
-                          true,
-                          _mainImage,
-                          "대표 이미지 가져오기",
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: _buildImagePreview(
-                          false,
-                          _detailImage,
-                          "상세 이미지 가져오기",
-                        ),
-                      ),
-                      const SizedBox(height: 50),
-                    ],
+                Text(
+                  '  권장크기 300 * 300 / 용량 : 10MB 이하 / 파일 형식 : PNG,JPG,GIF',
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: Colors.blueGrey,
                   ),
-                  const SizedBox(height: 50),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // DB 저장 전 밸리데이션 체크
-                        if (selectedBrand == null ||
-                            _mainImage == null) {
-                          CustomSnackbar().errorSnackBar(
-                            "입력 오류",
-                            "제조사 및 대표 이미지는 필수입니다.",
-                          );
-                        } else {
-                          CustomSnackbar().okSnackBar(
-                            "완료",
-                            "상품이 성공적으로 등록되었습니다.",
-                          );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(
-                          0xFF333333,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(8),
-                        ),
-                      ),
-                      child: const Text(
-                        "상품 등록",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                const SizedBox(height: 16),
+                _imageBox(
+                  title: '대표 이미지',
+                  image: mainImage,
+                  onTap: () =>
+                      _pickImage((f) => mainImage = f),
+                ),
+                const SizedBox(width: 16),
+                _imageBox(
+                  title: 'Top 이미지',
+                  image: topImage,
+                  onTap: () =>
+                      _pickImage((f) => topImage = f),
+                ),
+                const SizedBox(width: 16),
+                _imageBox(
+                  title: 'Side 이미지',
+                  image: sideImage,
+                  onTap: () =>
+                      _pickImage((f) => sideImage = f),
+                ),
+                const SizedBox(width: 16),
+                _imageBox(
+                  title: 'Back 이미지',
+                  image: backImage,
+                  onTap: () =>
+                      _pickImage((f) => backImage = f),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Text(
+                  '*  ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.red,
+                  ),
+                ),
+                Text(
+                  '제조사명',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            DropdownButtonFormField<String>(
+              value: selectedManufacturer,
+              hint: const Text('제조사를 선택하세요'),
+              items: manufacturers
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
                     ),
-                  ),
-                ],
+                  )
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedManufacturer = value;
+                });
+              },
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 14,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
+}
 
-  //
-  /// 🔹 이미지 프리뷰 및 버튼 위젯
-  Widget _buildImagePreview(
-    bool isMain,
-    XFile? imageFile,
-    String btnText,
-  ) {
-    return Column(
+/// 이미지 박스 위젯
+Widget _imageBox({
+  required String title,
+  required File? image,
+  required VoidCallback onTap,
+}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Stack(
       children: [
-        Stack(
-          children: [
-            Container(
-              height: 180,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(20),
+        Container(
+          width: 140,
+          height: 140,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: image == null
+                    ? const Icon(
+                        Icons.camera_alt,
+                        size: 40,
+                        color: Colors.white,
+                      )
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          12,
+                        ),
+                        child: Image.file(
+                          image,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
               ),
-              child: imageFile == null
-                  ? Icon(
-                      Icons.camera_alt_outlined,
-                      size: 40,
-                      color: Colors.grey[400],
-                    )
-                  : ClipRRect(
-                      borderRadius: BorderRadius.circular(
-                        20,
-                      ),
-                      child: Image.file(
-                        File(imageFile.path),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-            ),
-
-            // 이미지가 있을 때만 X 버튼 표시
-            if (imageFile != null)
-              Positioned(
-                top: 8,
-                right: 8,
-                child: GestureDetector(
-                  onTap: () {
-                    _confirmDelete(isMain);
-                    Get.back();
-                  },
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white54,
-                      shape: BoxShape.circle,
-                    ),
-                    child: const Icon(
-                      Icons.cancel,
-                      color: Colors.black87,
-                      size: 28,
-                    ),
+              Container(
+                height: 36,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
                   ),
                 ),
               ),
-            ///////////////////
-          ],
+            ],
+          ),
         ),
-        const SizedBox(height: 12),
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () => _pickImage(isMain),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF333333),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
+
+        // ❌ 아이콘 (이미지 있을 때만)
+        if (image != null)
+          Positioned(
+            top: 6,
+            right: 6,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.black54,
+                shape: BoxShape.circle,
               ),
-            ),
-            child: Text(
-              btnText,
-              style: const TextStyle(
+              child: const Icon(
+                Icons.close,
+                size: 18,
                 color: Colors.white,
-                fontSize: 12,
               ),
             ),
           ),
-        ),
       ],
-    );
-  }
-
-  //
-  // 삭제 로직 실행
-  void _confirmDelete(bool isMain) {
-    CustomSnackbar().showDialog(
-      "이미지 수정",
-      "이미지를 삭제하시겠습니까?",
-      onConfirm: () {
-        // 다이얼로그에서 'OK'를 눌렀을 때만 실제 데이터 삭제
-        setState(() {
-          if (isMain) {
-            _mainImage = null;
-          } else {
-            _detailImage = null;
-          }
-        });
-
-        setState(() {});
-      },
-    );
-  }
-
-  //
-  /// 🔹 공통 드롭다운 위젯
-  Widget _dropdown({
-    required String label, //드랍다운 타이틀
-    required String? value,
-    required List<String> items,
-    required ValueChanged<String?> onChanged,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: DropdownButtonFormField<String>(
-        value: value,
-        hint: Text(label),
-        items: items
-            .map(
-              (item) => DropdownMenuItem(
-                value: item,
-                child: Text(item),
-              ),
-            )
-            .toList(),
-        onChanged: onChanged,
-        decoration: InputDecoration(
-          labelText: label,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 12,
-            vertical: 14,
-          ),
-        ),
-      ),
-    );
-  }
-
-  //
+    ),
+  );
 }
