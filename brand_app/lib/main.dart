8b0b1eb73028adbea8356f271aa373068e5b5f17
sion <jx010102@naver.com>
2025-12-31 18:26:48 +0900

    결재창 완료

--- brand_app/lib/main.dart
+++ brand_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:brand_app/firebase_options.dart';
+import 'package:brand_app/view/request.dart';
 import 'package:brand_app/view/staff_main_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
           seedColor: Colors.deepPurple,
         ),
       ),
-      home: const StaffMainpage(),
+      home: const Request(),
     );
   }
 }
