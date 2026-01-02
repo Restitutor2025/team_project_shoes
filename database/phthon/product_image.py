3d79b48be655ce0e32a381ec4eeaacfada347e98
KorWeiss <signalizer@hanmail.net>
2026-01-02 15:07:57 +0900

    02-Jan-2026 Chansol, Park Modified product_image.py

--- database/phthon/product_image.py
+++ database/phthon/product_image.py
@@ -5,6 +5,14 @@ from pydantic import BaseModel
 import config
 import pymysql
 
+    #  product_image CRUD
+    #Create: 31/12/2025 15:06, Creator: Chansol, Park
+    #Update log: 
+    #  DUMMY 00/00/0000 00:00, 'Point X, Description', Creator: Chansol, Park
+    #Version: 1.0
+    #Dependency: 
+    #Desc: product_image CRUD
+    
 router = APIRouter()
 
 def connect():
@@ -19,29 +27,25 @@ def connect():
     return conn
 
 @router.get("/view")
-async def view(pid: int, position: str):
+async def view(pid: int, position: int):
+    conn = connect()
     try:
-        conn = connect()
         curs = conn.cursor()
         curs.execute(
-            """
-                SELECT path FROM ProductImage
-                WHERE pid = %s AND position = %s
-            """, (pid, position)
+            "SELECT image FROM ProductImage WHERE pid=%s AND position=%s",
+            (pid, position)
         )
         row = curs.fetchone()
-        conn.close()
-        if row and row["path"]:
+
+        if row and row["image"]:
             return Response(
-                content = row["path"],
-                media_type = "path/jpeg",
-                headers = {"Cache-Control":"no-cache, no-store, must-revalidate"}
+                content=row["image"],
+                media_type="image/png",
+                headers={"Cache-Control":"no-cache, no-store, must-revalidate"}
             )
-        else:
-            return{"result":"No path found"}
-    except Exception as e:
-        print("Error: ", e)
-        return {"result": "Error"}
+        return {"result": "No image found"}
+    finally:
+        conn.close()
         
 @router.post("/upload")
 async def upload(
@@ -50,7 +54,7 @@ async def upload(
         image_data = await file.read()
         conn = connect()
         curs = conn.cursor()
-        sql = "INSERT INTO ProductImage (pid, position, path) VALUES (%s, %s, %s)"
+        sql = "INSERT INTO ProductImage (pid, position, image) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE image = VALUES(image)"
         curs.execute(sql, (pid, position, image_data))
         conn.commit()
         conn.close()
@@ -83,8 +87,8 @@ async def updatewithimage(
         image_data = await file.read()
         conn = connect()
         curs = conn.cursor()
-        sql = "UPDATE ProductImage SET path = %s WHERE pid = %s AND position = %s"
-        curs.execute(sql, (position, image_data, pid))
+        sql = "UPDATE ProductImage SET image = %s WHERE pid = %s AND position = %s"
+        curs.execute(sql, (image_data, pid, position))
         conn.commit()
         conn.close()
         return {"result":"OK"}
